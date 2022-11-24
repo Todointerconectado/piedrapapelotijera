68a1e0cc50ac5164f903e92773532a035d243d2a
Francisco <106126465+Francisco-Carusso@users.noreply.github.com>
2022-11-24 16:28:11 -0300

    V1.0.2 Agregar class con JV

--- js/app.js
+++ js/app.js
@@ -1,128 +1,32 @@
-// 1 es piedra.
-// 2 es papel.
-// 3 es tijera. 
+function iniciarJuego () {
+    //Todas estas const toman la información de los botones del HTML
+    const rock = document.getElementById("stone"); 
+    const paper = document.getElementById("paper");
+    const scissors = document.getElementById("scissors");
 
-let player = 0;
-let pc = 2;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Todas estas const toman la información de los botones del HTML
-const rock = document.getElementById("rock"); 
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-const userScore = document.getElementById('user');
-const pcScore = document.getElementById('pc');
-
-userScore.innerHTML = 0;
-pcScore.innerHTML = 0;
-
-class Game {
-    constructor()  {
-        this.iniciar()
-        this.eventosDeUsuario()
-    }
-
-    iniciar() {
-        this.options = {
-            rock,
-            paper,
-            scissors
+    function agregarClass() {
+        let stoneInput = document.getElementById('stoneInput').checked;
+        let paperInput = document.getElementById('paperInput').checked;
+        let scissorsInput = document.getElementById('scissorsInput').checked;
+        
+        if (stoneInput === true){
+            rock.classList.toggle("personajeRed");
         }
-    }
-    
-    eventosDeUsuario() {
-        this.options.rock.addEventListener('click', this.eleccionDeUsuario.bind(this));
-        this.options.paper.addEventListener('click', this.eleccionDeUsuario.bind(this));
-        this.options.scissors.addEventListener('click', this.eleccionDeUsuario.bind(this));
-    }
-
-    cambiarEleccionANumero(opcionUsuario) {
-        switch (opcionUsuario) {
-            case 'rock':
-                return 1
-            case 'paper':
-                return 2
-            case 'scissors':
-                return 3
+        else if (paperInput === true){
+            paper.classList.toggle("personajeRed");
+        }
+        else if (scissorsInput === true){
+            scissors.classList.toggle("personajeRed");
         }
     }
 
-    cambiarNumeroAOpcion(usuario) {
-        switch (usuario) {
-            case 1:
-                return 'Rock'
-            case 2:
-                return 'Paper'
-            case 3:
-                return `Scissors`
-        }
-    }
-
-    cambiarPcAOpcion(pc) {
-        switch (pc) {
-            case 1:
-                return 'Rock'
-            case 2:
-                return 'Paper'
-            case 3:
-                return `Scissors`
-        }
-    }
-    
-    eleccionDeUsuario (ev) {
-        const opcionUsuario = ev.target.dataset.opcion
-        let usuario = this.cambiarEleccionANumero(opcionUsuario)
-        let pc = Math.floor(Math.random()* (3 + 1 - 1) + 1)
-    
-        if (pc === usuario) {
-            this.empate(usuario, pc)
-        } else if (usuario === 1 && pc === 3 || usuario === 2 && pc === 1 || usuario === 3 && pc === 2) {
-            this.ganaste(usuario, pc)
-        } else {
-            this.perdiste(usuario, pc)
-        }
-
-    }
-
-    empate (usuario, pc) {
-        let usuarioFinal = this.cambiarNumeroAOpcion(usuario)
-        let pcFinal = this.cambiarPcAOpcion(pc)
-        swal('TIE!', `You chose ${usuarioFinal} and pc chose ${pcFinal}`, 'error')
-    }
-
-    ganaste (usuario, pc) {
-        let usuarioFinal = this.cambiarNumeroAOpcion(usuario)
-        let pcFinal = this.cambiarPcAOpcion(pc)
-        userScore.innerHTML++
-        swal('YOU WIN!', `You chose ${usuarioFinal} and pc chose ${pcFinal}`, 'success')
-            
-    }
-
-    perdiste (usuario, pc) {
-        let usuarioFinal = this.cambiarNumeroAOpcion(usuario)
-        let pcFinal = this.cambiarPcAOpcion(pc)
-        pcScore.innerHTML++
-        swal('YOU LOSE :(', `You chose ${usuarioFinal} and pc chose ${pcFinal}`, 'error')
-           
-    }
+    // Detectamos el evento Click
+    rock.addEventListener('click', agregarClass);
+    paper.addEventListener('click', agregarClass);
+    scissors.addEventListener('click', agregarClass);
 }
 
-var juego = new Game()
 
+
+// Esperamos a que cargue todo el html y css para carjar el javascript
+window.addEventListener('load', iniciarJuego);
